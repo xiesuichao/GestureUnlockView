929021eddee0f65447fdde224eca5c72c4bbd54a
xiesuichao <xiesuichao@wxbtc.com>
2018-09-17 22:31:58 +0800

    上传截图

--- app/src/main/java/com/example/admin/gestureunlockview/MainActivity.java
+++ app/src/main/java/com/example/admin/gestureunlockview/MainActivity.java
@@ -17,24 +17,23 @@ public class MainActivity extends AppCompatActivity {
         gestureDrawView = findViewById(R.id.gd_gesture_draw);
         final GestureResultView gestureResultView = findViewById(R.id.gr_gesture_result);
 
-
+        //手势绘制监听
         gestureDrawView.setOnGestureDrawListener(new GestureDrawView.OnGestureDrawListener() {
             @Override
             public void gestureDraw(List<GestureCircle> checkedList) {
-                for (GestureCircle circle : checkedList) {
-                    PrintUtil.log("checkedList", circle.getPosition());
-                }
+                //设置手势绘制选中节点，显示手势绘制结果
                 gestureResultView.setCheckedList(checkedList);
             }
         });
 
+        //手势绘制错误监听
         gestureDrawView.setOnGestureErrorListener(new GestureDrawView.OnGestureErrorListener() {
             @Override
             public void gestureError() {
+                //清除手势绘制结果
                 gestureResultView.clearResult();
             }
         });
-
 
     }
 
